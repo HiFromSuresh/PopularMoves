47d4c06b9a1ab3e25d76908ae31dc23c1e2d8f5e
Suresh kumar <sureshssk2006@ymail.com>
2016-02-23 21:51:33 +0530

    Fix: Implement parcelable and restore from saveInstanceState() during orientation change

--- app/src/main/java/com/sureshssk2006/gmail/popularmoves/TmdbMovie.java
+++ app/src/main/java/com/sureshssk2006/gmail/popularmoves/TmdbMovie.java
@@ -1,11 +1,13 @@
 package com.sureshssk2006.gmail.popularmoves;
 
 import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by Administrator on 2/2/2016.
  */
-public class TmdbMovie {
+public class TmdbMovie implements Parcelable{
 
     String movieName;
     String posterPath;
@@ -16,13 +18,41 @@ public class TmdbMovie {
     final String POSTER_SIZE = "w185";
     final String LARGE_POSTER_SIZE = "w342";
 
+    String fullPosterPath;
+    String modifiedReleaseDate;
+    String modifiedVoteAverage;
+
+
     public TmdbMovie(String movieName, String posterPath, String overView, String releaseDate, String voteAverage) {
         this.posterPath = posterPath;
         this.movieName = movieName;
         this.overView = overView;
         this.releaseDate = releaseDate;
         this.voteAverage = voteAverage;
+        this.fullPosterPath = posterPath();
+        this.modifiedReleaseDate = getReleaseDate();
+        this.modifiedVoteAverage = getVoteAverage();
     }
+
+    protected TmdbMovie(Parcel in) {
+        movieName = in.readString();
+        overView = in.readString();
+        fullPosterPath = in.readString();
+        modifiedReleaseDate = in.readString();
+        modifiedVoteAverage = in.readString();
+    }
+
+    public static final Creator<TmdbMovie> CREATOR = new Creator<TmdbMovie>() {
+        @Override
+        public TmdbMovie createFromParcel(Parcel in) {
+            return new TmdbMovie(in);
+        }
+
+        @Override
+        public TmdbMovie[] newArray(int size) {
+            return new TmdbMovie[size];
+        }
+    };
 
     public String posterPath() {
 
@@ -63,5 +93,19 @@ public class TmdbMovie {
     public String getVoteAverage() {
         String voteAverageWithTotal = voteAverage + "/10";
         return voteAverageWithTotal;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(movieName);
+        dest.writeString(overView);
+        dest.writeString(fullPosterPath);
+        dest.writeString(modifiedReleaseDate);
+        dest.writeString(modifiedVoteAverage);
     }
 }
